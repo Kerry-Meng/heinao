764ee687e778c4936a8619edce01a40c275dc7b7
softtron <335961289@qq.com>
2019-11-09 17:43:38 +0800

    add hello world

--- src/main/java/com/softtron/pinmaoserver/controllers/IndexController.java
+++ src/main/java/com/softtron/pinmaoserver/controllers/IndexController.java
@@ -1,5 +1,8 @@
 package com.softtron.pinmaoserver.controllers;
 
 public class IndexController {
-	
+	public static void main(String[] args) {
+//		System.out.println("hello worlds2!");
+		System.out.println("hello world!");
+	}
 }
