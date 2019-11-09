4e69ab7a4b25de95bd2b77395e204b260ca96ff3
softtron <335961289@qq.com>
2019-11-09 17:51:34 +0800

    Hello world3

--- src/main/java/com/softtron/pinmaoserver/controllers/IndexController.java
+++ src/main/java/com/softtron/pinmaoserver/controllers/IndexController.java
@@ -3,6 +3,6 @@ package com.softtron.pinmaoserver.controllers;
 public class IndexController {
 	public static void main(String[] args) {
 //		System.out.println("hello worlds2!");
-		System.out.println("hello world!");
+		System.out.println("hello world3!");
 	}
 }
