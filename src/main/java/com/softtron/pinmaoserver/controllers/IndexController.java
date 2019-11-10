250c40b7f4277badc94cefc6733e387d0b58abfb
softtron <335961289@qq.com>
2019-11-10 11:37:13 +0800

    AAA change

--- src/main/java/com/softtron/pinmaoserver/controllers/IndexController.java
+++ src/main/java/com/softtron/pinmaoserver/controllers/IndexController.java
@@ -2,11 +2,6 @@ package com.softtron.pinmaoserver.controllers;
 
 public class IndexController {
 	public static void main(String[] args) {
-//		System.out.println("hello worlds2!");
-<<<<<<< HEAD
-		System.out.println("hello world3!");
-=======
-		System.out.println("hello world6! ");
->>>>>>> develop
+		System.out.println("Hello worldAAA!");
 	}
 }
